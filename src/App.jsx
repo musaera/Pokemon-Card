e3688b005b2ecfc094e29f073061ed6fe0274736
Nama Anda <email@anda.com>
2024-08-09 08:47:10 +0700

    Pokemon

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PokemonList from "./components/PokemonList/PokemonList";
+import PokemonItem from "./components/PokemonItem/PokemonItem";
+
+function App() {
+  return (
+    <>
+      <PokemonList />
+    </>
+  );
+}
+
+export default App;
